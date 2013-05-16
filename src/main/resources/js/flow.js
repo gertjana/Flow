0d8528a98cc62b64bc0465227f8cc8ed7f86dbfc
Gertjan Assies <gertjan.assies@gmail.com>
2013-05-16 11:52:09 +0200

    defaulted endpoint to rhcloud url

--- src/main/resources/js/flow.js
+++ src/main/resources/js/flow.js
@@ -1,5 +1,5 @@
 var bindCss = ".record-flow";
-var endPoint = "http://perth:9999/flow";
+var endPoint = "http://http://flow-gassies.rhcloud.com/flow";
 var session = "nosession";
 
 $(document).ready(function() {
