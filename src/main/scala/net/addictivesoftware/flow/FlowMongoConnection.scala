21fda3d1be8e9129902680d41f3057f5f91399e1
Gertjan Assies <gertjan.assies@gmail.com>
2013-05-20 22:42:01 +0200

    same typo, different place

--- src/main/scala/net/addictivesoftware/flow/FlowMongoConnection.scala
+++ src/main/scala/net/addictivesoftware/flow/FlowMongoConnection.scala
@@ -3,8 +3,8 @@ package net.addictivesoftware.flow
 import com.mongodb.casbah.Imports._
 
 object FlowMongoConnection {
-  val mongoUser       = Option(System.getenv("OPENSHIFT_MONGO_DB_USER")) getOrElse FlowProperties.getString("mongo-user")
-  val mongoPassword   = Option(System.getenv("OPENSHIFT_MONGO_DB_PASSWORD")) getOrElse FlowProperties.getString("mongo-password")
+  val mongoUser       = Option(System.getenv("OPENSHIFT_MONGODB_DB_USER")) getOrElse FlowProperties.getString("mongo-user")
+  val mongoPassword   = Option(System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD")) getOrElse FlowProperties.getString("mongo-password")
   val authRequired:Boolean = true
 
   def flowCollection = {
