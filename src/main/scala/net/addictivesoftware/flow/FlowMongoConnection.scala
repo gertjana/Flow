c4fcb98090c7f9cb53cfcf445c3402dc7f1bd65f
Gertjan Assies <gertjan.assies@gmail.com>
2013-05-20 15:28:17 +0200

    now authenticates when mongo is running in secure mode

--- src/main/scala/net/addictivesoftware/flow/FlowMongoConnection.scala
+++ src/main/scala/net/addictivesoftware/flow/FlowMongoConnection.scala
@@ -5,7 +5,7 @@ import com.mongodb.casbah.Imports._
 object FlowMongoConnection {
   val mongoUser       = Option(System.getenv("OPENSHIFT_MONGO_DB_USER")) getOrElse FlowProperties.getString("mongo-user")
   val mongoPassword   = Option(System.getenv("OPENSHIFT_MONGO_DB_PASSWORD")) getOrElse FlowProperties.getString("mongo-password")
-  val authRequired:Boolean = Option(System.getenv("OPENSHIFT_MONGO_DB_USER")) != None
+  val authRequired:Boolean = true
 
   def flowCollection = {
     val db = MongoConnection().getDB("flow")
