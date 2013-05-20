ad3bb478cdd5fd500f8dd4ce9666a6b9bf10b149
Gertjan Assies <gertjan.assies@gmail.com>
2013-05-20 22:52:27 +0200

    moved mongo host/port setting to FlowMongoConnection

--- src/main/scala/net/addictivesoftware/flow/FlowMongoConnection.scala
+++ src/main/scala/net/addictivesoftware/flow/FlowMongoConnection.scala
@@ -3,12 +3,14 @@ package net.addictivesoftware.flow
 import com.mongodb.casbah.Imports._
 
 object FlowMongoConnection {
+  val mongoHost = Option(System.getenv("OPENSHIFT_MONGODB_DB_HOST")) getOrElse FlowProperties.getString("mongo-host")
+  val mongoPort = Option(System.getenv("OPENSHIFT_MONGODB_DB_PORT")) getOrElse FlowProperties.getString("mongo-port") toInt
   val mongoUser       = Option(System.getenv("OPENSHIFT_MONGODB_DB_USER")) getOrElse FlowProperties.getString("mongo-user")
   val mongoPassword   = Option(System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD")) getOrElse FlowProperties.getString("mongo-password")
   val authRequired:Boolean = true
 
   def flowCollection = {
-    val db = MongoConnection().getDB("flow")
+    val db = MongoConnection(mongoHost, mongoPort).getDB("flow")
     if (authRequired) {
       db.authenticate(mongoUser, mongoPassword)
     }
--- src/main/scala/net/addictivesoftware/flow/Main.scala
+++ src/main/scala/net/addictivesoftware/flow/Main.scala
@@ -8,10 +8,10 @@ import net.addictivesotware.flow.FlowService
 
 object Main extends App with SprayCanHttpServerApp {
   //if we're on openshift take those values else from properties file
-  val mongoHost = Option(System.getenv("OPENSHIFT_MONGODB_DB_HOST")) getOrElse FlowProperties.getString("mongo-host")
-  val mongoPort = Option(System.getenv("OPENSHIFT_MONGODB_DB_PORT")) getOrElse FlowProperties.getString("mongo-port") toInt
+//  val mongoHost = Option(System.getenv("OPENSHIFT_MONGODB_DB_HOST")) getOrElse FlowProperties.getString("mongo-host")
+//  val mongoPort = Option(System.getenv("OPENSHIFT_MONGODB_DB_PORT")) getOrElse FlowProperties.getString("mongo-port") toInt
 
-  val mongo = MongoConnection(mongoHost, mongoPort)
+// val mongo = MongoConnection(mongoHost, mongoPort)
   
   val applicationHost = Option(System.getenv("OPENSHIFT_INTERNAL_IP"))   getOrElse FlowProperties.getString("application-host")
   val applicationPort = Option(System.getenv("OPENSHIFT_INTERNAL_PORT")) getOrElse FlowProperties.getString("application-port") toInt
