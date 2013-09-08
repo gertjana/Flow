fc647ae516fb2d70fa00cc8f2d0dcb74ca8d7f41
Gertjan Assies <gertjan.assies@gmail.com>
2013-09-08 14:44:04 +0200

    temp disabled logging

--- src/main/scala/net/addictivesoftware/flow/EventActor.scala
+++ src/main/scala/net/addictivesoftware/flow/EventActor.scala
@@ -1,19 +1,19 @@
 package net.addictivesoftware.flow
 
 import akka.actor.Actor
-import com.weiglewilczek.slf4s.Logging
+//import com.weiglewilczek.slf4s.Logging
 import net.addictivesoftware.flow.objects.{EventObject, WebEvent}
 
 sealed trait EventMessages
 case class RecordEvent(session:String, event:String, data:Map[String, String]) extends EventMessages
 
-class EventActor extends Actor with Logging {
+class EventActor extends Actor { //with Logging {
   def receive = {
     case RecordEvent(session, event, data) => {
       WebEvent.insert(new EventObject(session=session, event=event,data=data))
     }
     case _ => {
-      logger.error("Unhandled message sent to this actor")
+ //     logger.error("Unhandled message sent to this actor")
     }
   }
 }
